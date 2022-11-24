9d6d435997901556f8dbc6007d3b3488d32e7ded
mmm-nana <michi.171113@gmail.com>
2022-11-24 15:47:13 +0900

    check the test

---
+++ test.py
@@ -0,0 +1,29 @@
+import unittest
+from src import CSVPrinter
+
+class TestCSVPrinter(unittest.TestCase):
+    def test_read(self):
+        printer = CSVPrinter("/Users/nanako-m/Desktop/授業/特別講義A, C/課題2-2/sample.csv")
+        l = printer.read()
+        self.assertEqual(3, len(l))
+
+
+    def test_read2(self):
+        printer = CSVPrinter("/Users/nanako-m/Desktop/授業/特別講義A, C/課題2-2/sample.csv")
+        Line = printer.read()
+        self.assertEqual("5", Line[1][1])
+
+
+    def test_read3 (self):
+        # try:
+        #     printer = CSVPrinter("/Users/nanako-m/Desktop/授業/講義A, C/課題2-2/sample.csv")
+        #     printer.read()
+        #     unittest.TestCase.fail("This line should not be invoked")
+        # except FileNotFoundError as e:
+        #     raise e
+            # print("error:" + str(e))
+
+        with self.assertRaises(FileNotFoundError) as e:
+            printer = CSVPrinter("/Users/nanako-m/Desktop/授業/講義A, C/課題2-2/sample.csv")
+            line = printer.read()
+            
